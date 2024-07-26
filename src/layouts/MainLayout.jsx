1f4b12ee9d7d7393ea93aebd004715cc91eb4914
shahiur17 <mdshahinuri17@gmail.com>
2024-07-26 17:21:44 -0400

    added the footer section

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Header/Navbar';
+import Footer from '../components/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
            <Outlet></Outlet>
+           <Footer></Footer>
         </div>
     );
 };
